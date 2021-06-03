62e073d8601c893188c9f1a43b5fe39cb98e7c5d
omh18 <46792269+omh18@users.noreply.github.com>
2021-06-03 22:49:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-'use strict';
-
-const admin = require('firebase-admin');
+var admin = require("firebase-admin");
 const serviceAccount = require('serviceAccount.json');
 
-//admin.initializeApp({
-    //databaseURL: "https://create-active-inertia-default-rtdb.europe-west1.firebasedatabase.app",
-    //credential: admin.credential.cert(serviceAccount)
-//});
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://create-active-inertia-default-rtdb.europe-west1.firebasedatabase.app"
+});
 
 exports.handler = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;  //<---Important
-
-  if(admin.apps.length == 0) {   // <---Important!!! In lambda, it will cause double initialization.
-        admin.initializeApp({
-            databaseURL: "https://create-active-inertia-default-rtdb.europe-west1.firebasedatabase.app",
-            credential: admin.credential.cert(serviceAccount)
-        });
-  }
-
-  // Do whatever you want to do with firebase storage
-  // firebaseStorage = admin.storage();
-  
-  // Do whatever you want to do with firestore
-  // firebaseFirestore = admin.firestore();
-  
-  // Do whatever you want to do with auth
-  // firebaseFirestore = admin.auth();
-
-  // Do whatever you want to do with firebase
-
-  admin.database().ref('events').push(event);
-  
-  const response = {
-        statusCode: 200,
-        //body: JSON.stringify('Hello from Lambda!'),
-    };
-
-  return response;
+    let x = admin.database().ref('events');
+    x.push(event).then(function(){
+        callback(null, {statusCode: 200});
+    });
 };
-
